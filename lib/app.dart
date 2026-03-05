fb7b0a5d920aa2600811b2fe05d9c091b1bc80c2
ellina <angelinadevista344@gmail.com>
2026-03-05 18:41:12 +0700

    new

--- lib/app.dart
+++ lib/app.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
 
       /// HALAMAN AWAL
-      initialRoute: '/input_suratmasuk',
+      initialRoute: '',
 
       /// SEMUA ROUTE APLIKASI
       routes: {
